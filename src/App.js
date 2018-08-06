124f81be658cccb37093c1188bec4a86a61cac10
Sosodope <sonadolkar93@hotmail.com>
2018-08-06 22:32:37 +0700

    Set up trending results

--- src/App.js
+++ src/App.js
@@ -2,23 +2,51 @@ import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Results from "./components/Results";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, PageHeader } from "react-bootstrap";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    gifs: []
+  };
+  componentDidMount() {
+    axios
+      .get(`https://api.tenor.com/v1/trending?limit=48&key=Y1GGCZ8TPZAD`)
+      .then(res => {
+        this.setState({
+          gifs: res.data.results
+        });
+      });
+  }
   render() {
+    let smPadding = {
+      padding: 5
+    };
     return (
-      <Grid>
+      <Grid className="py3">
         <Row>
-          <Col>
-            <p>Hey</p>
+          <Col lg={12} style={smPadding}>
+            <PageHeader className="page-header">
+              Gif Love
+              <small className="subtitle"> give love, get love</small>
+            </PageHeader>
           </Col>
-          <Col>
+          <Col lg={12}>
             <SearchBar searchGifs={this.handleSearch} />
           </Col>
         </Row>
-        <div className="level-item has-text-centered">
-          <Results />
-        </div>
+        <h3>Trending Gifs</h3>
+        <Row>
+          {this.state.gifs.map(gif => {
+            return (
+              <Results
+                key={gif.id}
+                src={gif.media[0].tinygif.url}
+                link={gif.url}
+              />
+            );
+          })}
+        </Row>
       </Grid>
     );
   }
